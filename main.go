ccad37487f05f4ce4015115bedfe1f645ed69411
Nam Hai Nguyen <namhai.nguyen-ext@commercetools.com>
2024-03-08 15:07:56 +0000

    Update go mod
    Signed-off-by: Nam Hai Nguyen <namhai.nguyen-ext@commercetools.com>

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/prometheus/common/version"
+	"github.com/prometheus/client_golang/prometheus/collectors/version"
 
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
